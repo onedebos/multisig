86ac795774fb7541ecb1f3627bd86877d0da11f1
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2024-01-02 15:22:03 +0000

    add readme

---
+++ multisig.py
@@ -0,0 +1,109 @@
+import smartpy as sp
+
+
+@sp.module
+def main():
+    proposal_type: type = sp.big_map[
+        sp.int,
+        sp.record(
+            paymentAmt=sp.mutez,
+            receiver=sp.address,
+            voters=sp.set[sp.address],
+            votingComplete=sp.bool,
+        ),
+    ]
+
+    class MultiSigContract(sp.Contract):
+        def __init__(self, members, requiredVotes):
+            # Keep track of all the proposals submitted to the multisig
+            self.data.proposals = sp.cast(sp.big_map(), proposal_type)
+            self.data.activeProposalId = 0
+            self.data.members = sp.cast(members, sp.set[sp.address])
+            self.data.requiredVotes = sp.cast(requiredVotes, sp.nat)
+
+        @sp.entrypoint
+        def deposit(self):
+            assert self.data.members.contains(sp.sender), "Not a Member of MultiSig"
+
+        @sp.entrypoint
+        def submit_proposal(self, params):
+            """
+            Submit a new proposal/lambda for members
+            of the MultiSig to vote for.
+            """
+            assert self.data.members.contains(sp.sender), "Not a Member of MultiSig"
+            assert (
+                params.paymentAmt <= sp.balance
+            ), "The MultiSig does not have enough funds for this proposal"
+            self.data.activeProposalId += (
+                1  # submitting a new proposal inactivates the last one
+            )
+            self.data.proposals[self.data.activeProposalId] = sp.record(
+                paymentAmt=params.paymentAmt,
+                receiver=params.receiver,
+                voters=sp.set(sp.sender),
+                votingComplete=False,
+            )
+
+        @sp.entrypoint
+        def vote_on_proposal(self):
+            assert self.data.members.contains(sp.sender), "Not a Member of MultiSig"
+            # check if the user has previously voted on the proposal
+            assert not self.data.proposals[self.data.activeProposalId].voters.contains(
+                sp.sender
+            ), "Member has voted on this proposal"
+            self.data.proposals[self.data.activeProposalId].voters.add(sp.sender)
+            if (
+                sp.len(self.data.proposals[self.data.activeProposalId].voters)
+                == self.data.requiredVotes
+            ):
+                sp.send(
+                    self.data.proposals[self.data.activeProposalId].receiver,
+                    self.data.proposals[self.data.activeProposalId].paymentAmt,
+                )
+                self.data.proposals[self.data.activeProposalId].votingComplete = True
+
+
+@sp.add_test(name="Multisig")
+def test():
+    scenario = sp.test_scenario(main)
+    alice = sp.test_account("alice")
+    bob = sp.test_account("bob")
+    charlie = sp.test_account("charlie")
+    dani = sp.test_account("dani")
+    earl = sp.test_account("earl")
+    scenario.h3("MultiSig Proposal Contract")
+    members = sp.set([alice.address, bob.address, charlie.address, earl.address])
+
+    contract = main.MultiSigContract(members, 3)
+    scenario += contract
+
+    scenario.h3("Members can add funds to the contract")
+    contract.deposit().run(sender=alice.address, amount=sp.tez(50))
+
+    scenario.h3(
+        "Members can submit a proposal for funds to be sent to an address - Proposal 1."
+    )
+    contract.submit_proposal(
+        sp.record(paymentAmt=sp.tez(30), receiver=dani.address)
+    ).run(sender=alice.address)
+
+    scenario.h3("Non-members cannot vote on proposals")
+    contract.vote_on_proposal().run(valid=False, sender=dani.address)
+
+    scenario.h3("Member 2 can vote on proposal")
+    contract.vote_on_proposal().run(sender=bob.address)
+
+    scenario.h3("Member 3 can vote on proposal")
+    contract.vote_on_proposal().run(sender=charlie.address)
+
+    scenario.h3("Contract balance should drop to 20tez after transfer")
+    scenario.verify(contract.balance == sp.tez(20))
+
+    scenario.h3("A New proposal can be created")
+    contract.submit_proposal(
+        sp.record(paymentAmt=sp.tez(20), receiver=dani.address)
+    ).run(sender=alice.address)
+
+    scenario.h3("New proposal can be voted on")
+    contract.vote_on_proposal().run(sender=charlie.address)
---
+++ readme.md
@@ -0,0 +1,15 @@
+## MultiSig Contract for Tezos
+
+This is a [Tezos](https://tezos.com/) Multisig contract example written in SmartPy.
+
+To run the code in the **multisig.py** file, simple copy it, head over to the [SmartPy IDE](https://smartpy.io/ide), paste it and then hit the run button.
+
+To learn more about building on Tezos, vist [docs.tezos.com](https://docs.tezos.com).
+
+## Tools used
+
+- [SmartPy](https://smartpy.io/ide) - Deploy contracts on Tezos using an IDE.
+
+## More Tezos
+
+Learn more about building on [Tezos](https://docs.tezos.com)
